ba02c3d30c4310005be7de08cac46ee23e46f3be
PE-611 <PE-611@yandex.ru>
2020-10-20 15:25:17 +0300

    clean UART Rx

--- UART_Rx.v
+++ UART_Rx.v
@@ -1,45 +1,37 @@
 module UART_Rx (input clk, Rx,                     
-					 output  [7:0] data, 
+					 output reg [7:0] data, 
 					 output reg i				//for generation and checking UART_clk
 					 );
-
+					 
+parameter Fclk = 100 * 1000000;			// Input clk [Hz]
+parameter Fuart = 9600;						// recomended 230400, 115200, 57600, 38400, 33600, 28800, 19200, 14400, 9600, 1200.   		 	
+parameter divider	= (Fclk / (Fuart *2)) - 1;		
+					 
+initial data <= 8'b00000000;
 initial i <= 1'b0;												
-reg [14:0]	cnt;
-initial cnt <= 15'd0;				
+reg [15:0]	cnt;
+initial cnt <= 16'd0;				
 
 	always @(posedge clk) begin
 		cnt <= cnt + 1'b1;
-		if (cnt == 15'd5207) begin		   // generation UART_clk = 9600 Hz.
-		cnt <= cnt - 15'd5207;
-		i <= ~i;
+		if (cnt == divider) begin		   // generation UART_clk Hz.
+		cnt <= cnt - divider;
+		i <= ~i;									// UART clk
 		end
 	end
-
-	receive (.shift(data), .Rx_in(Rx), .UART_clk(i));
-	
-endmodule					
-					
-////////////////////////////////////////////////////////////////
-
-module receive (input UART_clk,
-					 input Rx_in,
-					 output reg [7:0] shift,
-					 output reg [2:0] g
-					);
 					
 reg l;											// receive flag
 initial l <= 1'b0;
-initial g <= 2'd0;  							// counter of the bits for recieve (8 bit)
-initial shift <= 7'b00000000;				// data shift reg
+reg [2:0] g;									// counter of the bits for recieve (8 bit)
 					
-	always @(posedge UART_clk) begin		// 
+	always @(posedge i) begin				// 
 			
-		if (Rx_in == 1'b0) begin			// detector of the UART start bit
+		if (Rx == 1'b0) begin				// detector of the UART start bit
 			l <= 1'b1;							// set receive flag 
 		end
 						
 		if (l == 1'b1) begin					// if and while the flag = 1, we write receive bits in bits in shift reg
-			shift <=  {shift[7:0], Rx_in};
+			data <=  {data[7:0], Rx};
 			g <= g + 1'b1;
 		end
 		
@@ -48,12 +40,6 @@ initial shift <= 7'b00000000;				// data shift reg
 			g <= g - 3'd7;						// -> clear counter of the bits for recieve and ->
 			
 		end
-
 	end
-
-
-
 		
 endmodule
-
-////////////////////////////////////////////////////////////////
