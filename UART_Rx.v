109e5191b5f675cef7f3857af16eeb632721928c
PE-611 <PE-611@yandex.ru>
2020-10-20 09:43:31 +0300

    UART_rx with shift reg

--- UART_Rx.v
+++ UART_Rx.v
@@ -1,5 +1,5 @@
 module UART_Rx (input clk, Rx,                     
-					 output  [0:7] data, 
+					 output  [7:0] data, 
 					 output reg i				//for generation and checking UART_clk
 					 );
 
@@ -27,9 +27,6 @@ module receive (input UART_clk,
 					 output reg [2:0] g
 					);
 					
-
-reg [2:0] z;									// counter of the data bits in shift reg
-initial z <= 1'd0;
 reg l;											// receive flag
 initial l <= 1'b0;
 initial g <= 2'd0;  							// counter of the bits for recieve (8 bit)
@@ -42,15 +39,13 @@ initial shift <= 7'b00000000;				// data shift reg
 		end
 						
 		if (l == 1'b1) begin					// if and while the flag = 1, we write receive bits in bits in shift reg
-			shift[z] <= Rx_in;
-			z <= z + 1'b1;
+			shift <=  {shift[7:0], Rx_in};
 			g <= g + 1'b1;
 		end
 		
 		if (g == 3'd7) begin				   // if number of the bits for recieve == 8, we ->
 			l <= ~l;								// -> reset receive flag, ->
 			g <= g - 3'd7;						// -> clear counter of the bits for recieve and ->
-			z <= z - 3'd7;						// -> clear counter of the data bits in shift reg.
 			
 		end
 
